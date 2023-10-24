df138260c5505784efe205c18ba5c81f4e4d52af
Nadia Koluzaeva <nadiakoluzaeva@gmail.com>
2023-10-23 21:54:23 -0400

    feat: fixtures changed (Mon, Oct 23, 2023 09:54 PM)

--- conftest.py
+++ conftest.py
@@ -10,7 +10,7 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope="session")
 def browser():
     chrome_options = Options()
-    chrome_options.add_argument("--no-sandbox")
+
     chrome_options.add_argument("--headless")  # Включить режим headless
     chrome_options.add_argument("--disable-gpu")  # Отключить использование GPU
     chrome_options.add_argument("--window-size=1920,1080")  # Установить разрешение окна
